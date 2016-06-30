f73973698ee0ee028099cde8eee7564c95006425
Gig77 <frech.christian@gmail.com>
2016-06-30 14:08:42 +0200

    save fitted .RData object

--- exomeCopy/get-cnv.R
+++ exomeCopy/get-cnv.R
@@ -105,6 +105,9 @@ if (sex == "m") {
 	fit[[opt$sample]][["chrY"]] <- fit.Y
 }
 
+# save fit
+save(fit, file=paste0("/mnt/projects/hdall/results/exomeCopy/", opt$sample, ".exomeCopy.fit.RData"))
+
 # plot results
 pdf(paste0(opt$sample, ".combined.pdf"), width=15, height=7)
 for (chr in c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY")) {
